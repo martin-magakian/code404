0d010ff40e9f7286a8911e283b55d400a8745f6c
lukomboA <32895472+lukomboA@users.noreply.github.com>
2017-10-24 14:02:06 +0200

    maj

--- static_html/contact.php
+++ static_html/contact.php
@@ -22,36 +22,44 @@
     	<form class="form-contat">
 		  <div class="row">
 		    <div class="col">
-		      <input type="text" class="form-control" placeholder="Nom">
+		      <input type="text" class="form-control text-center" placeholder="Nom">
 		    </div>
 		    <div class="col">
-		      <input type="text" class="form-control" placeholder="Prénom(s)">
+		      <input type="text" class="form-control text-center" placeholder="Prénom(s)">
 		    </div>
 			</div>
 			<br/>
 			<div class="row">
 				<div class="col">
-			    		<input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+			    		<input type="email" class="form-control text-center" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="E-mail">
 			    		<small id="emailHelp" class="form-text text-muted">Cet e-mail sera conservé seulement pour une inscription.</small>
 				</div>
-		</div>
+			</div>
 		<br/>
 			<div class="phone-list">
 				
 					<div class="input-group phone-input">
 						<span class="input-group-btn">
-							<button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-expanded="false"><span class="type-text">Type</span> <span class="caret"></span></button>
+							<button type="button" class="btn btn-default dropdown-toggle ctct" data-toggle="dropdown" aria-expanded="false"><span class="type-text">Type</span> <span class="caret"></span></button>
 							<ul class="dropdown-menu" role="menu">
-								<li><a class="changeType" href="javascript:;" data-type-value="phone">Phone</a></li>
-								<li><a class="changeType" href="javascript:;" data-type-value="fax">Fax</a></li>
+								<li><a class="changeType" href="javascript:;" data-type-value="phone">Fix</a></li>
+								<!-- <li><a class="changeType" href="javascript:;" data-type-value="fax">Fax</a></li> -->
 								<li><a class="changeType" href="javascript:;" data-type-value="mobile">Mobile</a></li>
 							</ul>
 						</span>
 						<input type="hidden" name="phone[1][type]" class="type-input" value="" />
-						<input type="text" name="phone[1][number]" class="form-control" placeholder="+1 (999) 999 9999" />
+						<input type="text" name="phone[1][number]" class="form-control text-center" placeholder="Téléphone" />
 					</div>
 					
 				</div>
+			<br/>
+				<div class="row">
+					<div class="col">
+						 <textarea class="form-control" id="exampleFormControlTextarea1" rows="8" placeholder="Message"></textarea>
+					</div>
+					
+				</div>
+				<button type="submit" class="btn btn-primary btn-primary-about droite-contact"><a href="/?page=code404" class="contaact">Submit</a></button>
 		</form>
       
     </div>
